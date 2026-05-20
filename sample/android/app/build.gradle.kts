0fe6177dc8c09409e40637cadb156e831c3fae8f
nich.otieno <nich.otieno@gmail.com>
2026-05-20 23:25:22 +0300

    chore(deps): update dependencies and lockfile versions

--- sample/android/app/build.gradle.kts
+++ sample/android/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         applicationId = "com.smileidentity.flutter.sample"
-        minSdk = 21
+        minSdk = flutter.minSdkVersion
         targetSdk = 36
         versionCode = 1
         versionName = "1.0.0"
