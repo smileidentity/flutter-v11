7b244f2edb2c7515973d51ebcd7c7afb067eef62
nich.otieno <nich.otieno@gmail.com>
2026-05-21 10:00:07 +0300

    feat: add plugin management and dependency resolution configuration in settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,1 +1,24 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("com.android.library") version "8.9.2"
+        id("org.jetbrains.kotlin.android") version "2.2.21"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.2.21"
+        id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://central.sonatype.com/repository/maven-snapshots/") }
+    }
+}
+
 rootProject.name = "smile_id_flutter"
